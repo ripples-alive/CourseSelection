4bc5ca91ebbce26f7262d535b20ab737597347c5
JayvicWen <564311873@qq.com>
2014-07-17 12:05:17 +0800

    Patch

---
+++ log.py
@@ -0,0 +1,25 @@
+import time
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+def write_log(content):
+    """Write content into log file."""
+    log = open('.log', 'a')
+    log.write(time.strftime('%Y-%m-%d %H:%M:%S'))
+    log.write(' : ')
+
+    # if type(content) == type(''):
+    if isinstance(content, str):
+        log.write(content + '\n')
+    elif content is not None:
+        for row in content:
+            for item in row:
+                log.write(item)
+                log.write('\t')
+            log.write('\n')
+
+    log.write('\n')
+    log.close()
